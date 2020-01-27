9c332a5c6da6d3ddd3151a8a80239c112732d92e
Arturo Guerra <arturoguerra360@gmail.com>
2020-01-27 06:42:34 +0000

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# fireteamsupport-frontend
-Nuxt.js frontend for fireteamsupport.net
+# destinyarena
+
+> Destiny Arena Landing page and authentication website
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm run install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/registration.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="body">
+    <div class="main-flex-container">
+      <div class="multi">
+        <div class="single">
+          //TODO: Discord
+        </div>
+        <div class="single">
+          //TODO: Faceit
+        </div>
+      </div>
+      <div class="multi">
+        <div class="single">
+          //TODO: Steam
+        </div>
+        <div class="single">
+          //TODO: PSN
+        </div>
+        <div class="single">
+          //TODO: Xbox
+        </div>
+      </div>
+      <div class="single captcha">
+        //TODO: Captcha
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
