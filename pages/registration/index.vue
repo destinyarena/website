8806462764578d9a6d84e3c0ef97e219af8e484d
Arturo Guerra <ar2roguerra@gmail.com>
2020-02-01 06:21:03 +0000

    Redirects are working

--- pages/registration/index.vue
+++ pages/registration/index.vue
@@ -1,38 +1,81 @@
 <template>
   <div class="body">
-    <div class="registration-title">
-      <img src="~/assets/images/logo.jpg"></img>
-      <span>REGISTRATION</span>
-    </div>
-    <div class="main-flex-container">
-        <div class="single-button">
-          DISCORD
-          {{ tokens.discord }}
-          <!--img src="~/assets/images/discord.svg"></img-->
-        </div>
-        <div class="single-button">
-          FACEIT
-          <!--img src="~/assets/images/faceit.png"></img-->
-        </div>
-        <div class="single-button">
-          BUNGIE
-          <!--img src="~/assets/images/bungie.png"></img-->
-        </div>
-    </div>
-    <div class="registration">
-        <recaptcha
-          @error="onError"
-          @success="onSuccess"
-          @expired="onExpired"
-        />
-        <div class="buttons row">
-          <div class="reset-button">
-            Reset
+    <div class="section">
+
+      <!--
+        Discord
+      -->
+      <div class="tile">
+        <div class="tile-header discord-branding">
+          <div class="tile-header-icon">
+            <img src="@/assets/images/discord.svg"></img>
           </div>
-          <div class="register-button">
-            Register
+
+          <div class="tile-header-text">
+            <span>DISCORD</span>
           </div>
         </div>
+
+        <div class="tile-body">
+          <span>Username: Arturo#0001</span>
+        </div>
+
+        <div class="tile-footer">
+          <button v-on:click="discordClick" class="button">
+            <span>LOGIN</span>
+          </button>
+        </div>
+      </div>
+
+      <!--
+        Bungie
+      -->
+      <div class="tile">
+        <div class="tile-header bungie-branding">
+          <div class="tile-header-icon">
+            <img src="@/assets/images/bungie.png"></img>
+          </div>
+
+          <div class="tile-header-text">
+            <span>BUNGIE</span>
+          </div>
+        </div>
+
+        <div class="tile-body">
+          <span>Username: Arturo#0001</span>
+        </div>
+
+        <div class="tile-footer">
+          <button v-on:click="faceitClick" class="button">
+            <span>LOGIN</span>
+          </button>
+        </div>
+      </div>
+
+      <!--
+        Faceit
+      -->
+      <div class="tile">
+        <div class="tile-header faceit-branding">
+          <div class="tile-header-icon">
+            <img src="@/assets/images/faceit.png"></img>
+          </div>
+
+          <div class="tile-header-text">
+            <span>FACEIT</span>
+          </div>
+        </div>
+
+        <div class="tile-body">
+          <span>Username: Arturo#0001</span>
+        </div>
+
+        <div class="tile-footer">
+          <button v-on:click="faceitClick" class="button">
+            <span>LOGIN</span>
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -48,15 +91,18 @@ export default {
       },
       profiles: {
         discord: {
+          show: false,
           id: '',
           username: '',
           discriminator: ''
         },
         faceit: {
+          show: false,
           id: '',
           username: ''
         },
         bungie: {
+          show: false,
           id: '',
           username: '',
           xbox: '',
@@ -74,8 +120,40 @@ export default {
     populate () {
       if (process.client) {
         this.tokens.discord = this.$nuxt.$store.state.auth.discord
+        this.tokens.bungie = this.$nuxt.$store.state.auth.bungie
+        this.tokens.faceit = this.$nuxt.$store.state.auth.faceit
       }
     },
+
+    async discordClick (event) {
+      try {
+        const { data } = await this.$nuxt.$axios.get('/oauth/discord/authurl')
+        console.log(data)
+        window.location.href = data
+      } catch (e) {
+        console.error(e)
+      }
+    },
+    async bungieClick (event) {
+      try {
+        const { data } = await this.$nuxt.$axios.get('/oauth/bungie/authurl')
+        console.log(data)
+        window.location.href = data
+      } catch (e) {
+        console.error(e)
+      }
+    },
+    async faceitClick (event) {
+      try {
+        const { data } = await this.$nuxt.$axios.get('/oauth/faceit/authurl')
+        console.log(data)
+        window.location.href = data
+      } catch (e) {
+        console.error(e)
+      }
+    },
+
+    // Recaptcha Stuff
     onError (error) {
       console.log(error)
     },
