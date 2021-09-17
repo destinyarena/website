32939f83deff1dc659b40fa780acdb2354678572
Arturo Guerra <ar2roguerra@gmail.com>
2021-09-17 13:32:23 -0500

    Updated deps

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,86 +1,79 @@
-
-module.exports = {
-  mode: 'universal',
-  /*
-  ** Headers of the page
-  */
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Destiny Arena',
+    title: 'DestinyArena',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Registration website for Destiny Arena' }
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.jpg' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  /*
-  ** Customize the progress-bar color
-  */
+
   loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@assets/scss/index.scss'
   ],
-  /*
-  ** Plugins to load before mounting the App
-  */
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    '@nuxtjs/sentry',
-    // Doc: https://axios.nuxtjs.org/usage
-    ['@nuxtjs/axios', {
-      prefix: '/api',
-      port: '443',
-      host: 'destinyarena.app',
-      https: true
-    }],
 
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    '@nuxtjs/dotenv',
+    '@nuxtjs/recaptcha',
     ['nuxt-vuex-localstorage', {
       localStorage: ['auth'],
       sessionStorage: ['auth']
     }],
-
-    // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv',
-    // Doc: https://github.com/nuxt-community/recaptcha-module
-    ['@nuxtjs/recaptcha', {
-      size: 'normal',
-      siteKey: '6LcIWdMUAAAAAEO8tZI7KCdQOKBup1JWIPHCsUgz',
-      version: 2
-    }]
   ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+      prefix: '/api',
+      port: '443',
+      host: 'destinyarena.app',
+      https: true,
+  },
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  recaptcha: {
+    size: 'normal',
+    siteKey: '6LcIWdMUAAAAAEO8tZI7KCdQOKBup1JWIPHCsUgz',
+    version: 2
+  },
+
   sentry: {
     dsn: "https://a8173b6bdf9446a29752775735d811f2@o471645.ingest.sentry.io/5503927",
+  },
 
-  },
-  /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {
-  },
-  /*
-  ** Build configuration
-  */
+  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    /*
-    ** You can extend webpack config here
-    */
     extend (config, ctx) {
       config.module.rules.push({
         test: /\.(ogg|mp3|wav|mpe?g)$/i,
@@ -90,5 +83,5 @@ module.exports = {
         }
       })
     }
-  }
+  } 
 }
